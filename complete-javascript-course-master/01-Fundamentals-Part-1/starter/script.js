0bb429f2afed2988e1088f1348f8a46f1dc8798d
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-07-19 17:58:54 -0400

    saving very beginner basic notes

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,27 @@
 let js = 'amazing';
 if (js === 'amazing') alert('JavaSCript is FUN!');
-console.log(40 + 8 + 23 - 10);
+
+// Number
+let age = 23;
+
+// String
+let firstName = 'Jonas';
+
+// Boolean
+let rainyDay = true;
+
+// Undefined
+let children;
+
+// Null
+let noValue = null;
+
+// Symbol (ES6 - 2015)
+const symbolExample = Symbol('sybol');
+
+// Bigint, either appen n to a string or call BigInt()
+const hugeNumber = 98492109318391012029n;
+const alsoHuge = BigIng(98492109318391012029);
+
+// Object
+const objectExample = {key: 'value'};
