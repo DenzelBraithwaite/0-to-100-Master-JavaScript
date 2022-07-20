8a995aa01514f5afb2155d88875eaf9a921d007c
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-07-20 09:39:59 -0400

    finished section 5

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// Section 5 challenge
+
+const dataOne = [17, 21, 23];
+const dataTwo = [12, -5, 0, 4];
+
+const printForecast = array => {
+    let finalString = '... ';
+    for (let i = 0; i < array.length; i++) {
+        finalString += `${array[i]}°C in ${i} days... `;
+    }
+    console.log(finalString);
+};
+
+printForecast(dataTwo);
