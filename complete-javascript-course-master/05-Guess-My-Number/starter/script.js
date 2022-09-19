7ff7263eab97b8dd1583afb99a805f4478de51ca
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-09-19 05:09:31 -0400

    added event listener for check box

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const check = document.querySelector('.check');
+
+check.addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    console.log('Please enter a value');
+  } else {
+    console.log(guess);
+  }
+});
