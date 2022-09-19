9b67f00371ab120a0ebe9d6f1ae87d805c509d90
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-09-19 06:06:54 -0400

    first draft of functional game

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,13 +1,56 @@
 'use strict';
 
 const check = document.querySelector('.check');
+// let message = document.querySelector('.message').textContent;
+let secretNumber = Math.ceil(Math.random() * 20);
+const number = document.querySelector('.number').textContent;
+
+let currentScore = 20;
+let currentHighscore = 0;
+const resetButton = document.querySelector('.again');
+
+document.querySelector('.number').textContent = secretNumber;
+
+const setHighscore = score => {
+  if (score > currentHighscore) {
+    currentHighscore = score;
+    document.querySelector('.highscore').textContent = currentScore;
+  }
+};
+
+const setScore = () => {
+  document.querySelector('.score').textContent = currentScore;
+};
+
+const resetGame = () => {
+  secretNumber = Math.ceil(Math.random() * 20);
+  currentScore = 20;
+
+  document.querySelector('.number').textContent = secretNumber;
+  setScore();
+};
+
+const compareNumber = guess => {
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      '🛑 Did you enter a number?';
+  } else if (guess === secretNumber) {
+    setHighscore(currentScore);
+    document.querySelector('.message').textContent = 'Congrats!🎉🎉🎉';
+  } else if (guess > secretNumber) {
+    currentScore--;
+    setScore(currentScore);
+    document.querySelector('.message').textContent = 'Too high👇🏾👇🏾👇🏾';
+  } else {
+    currentScore--;
+    setScore(currentScore);
+    document.querySelector('.message').textContent = 'Too low☝🏾☝🏾☝🏾';
+  }
+};
 
 check.addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-
-  if (!guess) {
-    console.log('Please enter a value');
-  } else {
-    console.log(guess);
-  }
+  compareNumber(guess);
 });
+
+resetButton.addEventListener('click', resetGame);
