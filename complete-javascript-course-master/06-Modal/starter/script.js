3c24eafacaccad46bc1a600d8e5e41c63fe7a11c
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-09-21 06:08:01 -0400

    selected elements, starting small project

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overaly');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+console.log(btnOpenModal);
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(btnOpenModal[i].textContent);
+}
