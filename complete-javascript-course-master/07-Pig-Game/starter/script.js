de2cde8adb189564f53d6d813c0e8752688f2587
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-09-22 05:45:14 -0400

    customized color palette

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,3 +1,8 @@
 'use strict';
 
 // Change the design a bit before starting.
+let player1 = document.getElementById('name--0');
+let player2 = document.getElementById('name--1');
+
+// player1.textContent = prompt('Enter player 1 name');
+// player2.textContent = prompt('Enter player 2 name');
