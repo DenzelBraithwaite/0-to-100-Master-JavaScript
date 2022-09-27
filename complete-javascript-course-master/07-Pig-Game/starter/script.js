f8bdbb4496185e93c20a9f211e2fe52f90f04e80
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-09-27 06:10:27 -0400

    modifying and refactoring game logic, using dynamic classes

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,15 +1,15 @@
 'use strict';
 
 // Player 1
-const player1Name = document.querySelector('#name--1');
-let player1currentPoints = document.querySelector('#current--1');
-let player1TotalScore = document.querySelector('#score--1');
+const player1Name = document.querySelector('#name--0');
+let player1currentPoints = document.querySelector('#current--0');
+let player1TotalScore = document.querySelector('#score--0');
 let player1Points = 0;
 
 // Player 2
-const player2Name = document.getElementById('name--2');
-let player2currentPoints = document.getElementById('current--2');
-let player2TotalScore = document.querySelector('#score--2');
+const player2Name = document.getElementById('name--1');
+let player2currentPoints = document.getElementById('current--1');
+let player2TotalScore = document.querySelector('#score--1');
 let player2Points = 0;
 
 const dice = document.querySelector('.dice');
@@ -17,20 +17,20 @@ const holdBtn = document.querySelector('.btn--hold');
 const restartBtn = document.querySelector('.btn--restart');
 let currentPoints = 0;
 let roll = 1;
-let player1Turn = true;
-// let gameOver = false;
+let activePlayer = 0;
 
 // player1Name.textContent = prompt('Enter player 1 name');
 // player2Name.textContent = prompt('Enter player 2 name');
 
-// To create roll effect
-const sleepNow = delay => new Promise(resolve => setTimeout(resolve, delay));
-const switchTurn = boolean => {
-  return !boolean;
+const switchTurn = () => {
+  return activePlayer ? (activePlayer = 0) : (activePlayer = 1);
 };
 
+// To create roll effect
+const sleepNow = delay => new Promise(resolve => setTimeout(resolve, delay));
+
 const hold = () => {
-  if (player1Turn) {
+  if (activePlayer === 0) {
     player1Points += currentPoints;
     player1TotalScore.textContent = player1Points;
   } else {
@@ -39,13 +39,14 @@ const hold = () => {
   }
 
   currentPoints = 0;
-  player1Turn = switchTurn(player1Turn);
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  switchTurn();
 };
 
 const restart = function () {
   document.querySelector('body').style.backgroundImage =
     'linear-gradient(to top left, #20c997 0%, #c3fae8 100%)';
-  player1Turn = true;
+  activePlayer = 0;
   currentPoints = 0;
   player1Points = 0;
   player2Points = 0;
@@ -64,25 +65,21 @@ async function rollDice() {
 
   if (roll === 1) {
     currentPoints = 0;
-    player1Turn = switchTurn(player1Turn);
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    switchTurn();
   } else {
     currentPoints += roll;
   }
 
-  if (player1Turn) {
-    player2currentPoints.textContent = 0;
-    player1currentPoints.textContent = currentPoints;
-  } else {
-    player1currentPoints.textContent = 0;
-    player2currentPoints.textContent = currentPoints;
-  }
+  document.querySelector(`#current--${activePlayer}`).textContent =
+    currentPoints;
 
-  if (player1Turn && currentPoints + player1Points >= 30) {
+  if (!activePlayer && currentPoints + player1Points >= 30) {
     player1Points += currentPoints;
     player1TotalScore.textContent = player1Points;
     document.querySelector('body').style.backgroundImage =
       'linear-gradient(to top left, red 0%, blue 100%)';
-  } else if (!player1Turn && currentPoints + player2Points >= 30) {
+  } else if (activePlayer && currentPoints + player2Points >= 30) {
     player2Points += currentPoints;
     player2TotalScore.textContent = player2Points;
     document.body.style.backgroundImage =
