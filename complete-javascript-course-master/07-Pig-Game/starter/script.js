eb61b7beee1bce5238ab6fcba4f7970e1b2142b8
Denzel Braithwaite <braithwaite.de@gmail.com>
2022-09-27 07:28:05 -0400

    game complete, might refactor and polish certain things

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -5,26 +5,30 @@ const player1Side = document.querySelector('.player--0');
 const player1Name = document.querySelector('#name--0');
 let player1currentPoints = document.querySelector('#current--0');
 let player1TotalScore = document.querySelector('#score--0');
-let player1Points = 0;
 
 // Player 2
 const player2Side = document.querySelector('.player--1');
 const player2Name = document.getElementById('name--1');
 let player2currentPoints = document.getElementById('current--1');
 let player2TotalScore = document.querySelector('#score--1');
-let player2Points = 0;
 
 // Game controls / buttons
 const dice = document.querySelector('.dice');
 const holdBtn = document.querySelector('.btn--hold');
 const restartBtn = document.querySelector('.btn--restart');
 
+// Holds each player's scores, starting with player 1
+const playerScores = [0, 0];
+
 let currentPoints = 0;
 let roll = 1;
 let activePlayer = 0;
+let winner;
 
 // Change player turn handler
 const switchTurn = () => {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+
   activePlayer = activePlayer === 0 ? 1 : 0;
   player1Side.classList.toggle('player--active');
   player2Side.classList.toggle('player--active');
@@ -53,33 +57,45 @@ async function rollDice() {
 }
 
 const hold = () => {
-  if (activePlayer === 0) {
-    player1Points += currentPoints;
-    player1TotalScore.textContent = player1Points;
-  } else {
-    player2Points += currentPoints;
-    player2TotalScore.textContent = player2Points;
-  }
-
+  playerScores[activePlayer] += currentPoints;
   currentPoints = 0;
-  document.querySelector(`#current--${activePlayer}`).textContent = 0;
-  switchTurn();
+  document.querySelector(`#score--${activePlayer}`).textContent =
+    playerScores[activePlayer];
+
+  if (playerScores[activePlayer] >= 100) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+    winner = activePlayer;
+    if (winner === 0) {
+      player2Side.classList.add('player--loser');
+    } else {
+      player1Side.classList.add('player--loser');
+    }
+  } else {
+    switchTurn();
+  }
 };
 
 const restart = function () {
   activePlayer = 0;
   currentPoints = 0;
-  player1Points = 0;
-  player2Points = 0;
+  playerScores[0] = 0;
+  playerScores[1] = 0;
   player1TotalScore.textContent = 0;
   player1currentPoints.textContent = 0;
   player2currentPoints.textContent = 0;
   player2TotalScore.textContent = 0;
 
-  document.querySelector('body').style.backgroundImage =
-    'linear-gradient(to top left, #20c997 0%, #c3fae8 100%)';
-  document.querySelector('.player--1').classList.add('player--active');
-  document.querySelector('.player--2').classList.remove('player--active');
+  document.querySelector('.player--0').classList.add('player--active');
+  document.querySelector('.player--1').classList.remove('player--active');
+  document.querySelector('.player--0').classList.remove('player--winner');
+  document.querySelector('.player--1').classList.remove('player--winner');
+  player1Side.classList.remove('player--loser');
+  player2Side.classList.remove('player--loser');
 };
 
 dice.addEventListener('click', rollDice);
